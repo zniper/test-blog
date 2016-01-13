0487e837828ce8739e8b426d70aa7d876156945b
Zniper <me.zniper@gmail.com>
2016-01-14 00:42:10 +0700

    Add page for listing entries by editor or category

--- src/content/urls.py
+++ src/content/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^new-entry/$', views.PostEntryView.as_view(), name='post-entry'),
+    url(r'^entries/$', views.EntryListView.as_view(), name='entry-list'),
     url(r'^entry-(?P<pk>\d+)/$', views.SingleEntryView.as_view(),
         name='view-entry'),
 ]
--- src/content/views.py
+++ src/content/views.py
@@ -1,14 +1,15 @@
-from django.views.generic import CreateView, DetailView
+from django.views import generic
 from django.core.urlresolvers import reverse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages import success
 from django.utils.translation import ugettext_lazy as _
+from django.core.exceptions import FieldError
 
 from .models import Entry
 from .forms import EntryForm
 
 
-class PostEntryView(LoginRequiredMixin, CreateView):
+class PostEntryView(LoginRequiredMixin, generic.CreateView):
     model = Entry
     form_class = EntryForm
 
@@ -24,5 +25,20 @@ class PostEntryView(LoginRequiredMixin, CreateView):
         return reverse('content:view-entry', kwargs={'pk': self.object.pk})
 
 
-class SingleEntryView(DetailView):
+class SingleEntryView(generic.DetailView):
     model = Entry
+
+
+class EntryListView(generic.ListView):
+    template_name = "content/entries.html"
+    model = Entry
+    paginate_by = 3
+
+    def get_queryset(self):
+        """Additionally filter the result by given field"""
+        queryset = super(EntryListView, self).get_queryset()
+        filter_by = self.request.GET.get('by')
+        target_id = self.request.GET.get('id')
+        if filter_by and target_id:
+            queryset = queryset.filter(**{filter_by: target_id})
+        return queryset
